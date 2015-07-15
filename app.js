ec1060e184c1385f8aab70018acfd69a3de82918
Mircea Alexandru <mircea.alexandru@gmail.com>
2015-07-15 15:31:24 +0300

    first draft

---
+++ app.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var _ = require('lodash')
+
+module.exports = function ( options ) {
+  var seneca = this
+  var plugin = 'seneca-auth-redirect'
+
+  function redirect(args, cb){
+    var req = args.req
+    var res = args.res
+    var kind = args.kind
+
+    var redirect = false
+    var ct = (req.headers['content-type']||'').split(';')[0]
+
+    if( options.redirect.always ) {
+      redirect = true
+    }
+    else if( !_.isUndefined(req.query.redirect) ) {
+      redirect = S(req.query.redirect).toBoolean()
+    }
+    else if( 'application/x-www-form-urlencoded' == ct || 'multipart/form-data' == ct ) {
+      redirect = true
+    }
+    else if( 'application/json' == ct ) {
+      redirect = false
+    }
+    else redirect = true;
+
+    if( redirect ) {
+      redirect = {
+        win:  _.isString(req.query.win) ? req.query.win : (options.redirect[kind]? options.redirect[kind].win: undefined) ,
+        fail: _.isString(req.query.fail) ? req.query.fail : (options.redirect[kind]? options.redirect[kind].fail: undefined)
+      }
+    }
+
+    cb(null, redirect)
+  }
+
+  seneca.add({role: 'auth', cmd: 'redirect'}, redirect)
+
+  return {
+    name:plugin
+  }
+}
